7453a2960342e9cf09995fb9092708db40408aa2
Akash <hafejmohammadakash@gmail.com>
2025-05-04 11:54:24 +0600

    all routes setup

--- src/layout/MainLayoute.jsx
+++ src/layout/MainLayoute.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { Outlet } from 'react-router-dom';
 
 const MainLayoute = () => {
     return (
         <div>
             {/* Navber */}
             <Navbar />
+
+            {/* Outlet */}
+            <div className='w-11/12 mx-auto'>
+                <Outlet />
+            </div>
             
         </div>
     );
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import MainLayoute from "../layout/MainLayoute";
+import Home from "../pages/Home";
+import AllTreatments from "../pages/AllTreatments";
+import MyProfile from "../pages/MyProfile";
+import MyAppointments from "../pages/MyAppointments";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayoute />
+    element: <MainLayoute />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/allTreatments",
+        element: <AllTreatments />,
+      },
+      {
+        path: "/myProfile",
+        element: <MyProfile />,
+      },
+      {
+        path: "myAppointments",
+        element: <MyAppointments />,
+      },
+    ],
   },
 ]);
 
