371c0f377801cfd230a2865008e7ab545d009426
Akash <hafejmohammadakash@gmail.com>
2025-05-04 13:25:58 +0600

    heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import Banner from '../components/Banner';
-import { useLoaderData } from 'react-router-dom';
-import ServiceCard from '../components/ServiceCard';
+import React from "react";
+import Banner from "../components/Banner";
+import { useLoaderData } from "react-router-dom";
+import ServiceCard from "../components/ServiceCard";
 
 const Home = () => {
-    const services = useLoaderData()
-    console.log(services)
+  const services = useLoaderData();
+  console.log(services);
 
+  return (
+    <div>
+      {/* Banner */}
+      <Banner />
 
-
-    return (
-        <div>
-            {/* Banner */}
-            <Banner />
-
-            {/* Our service  */}
-            <div className='grid gap-3 my-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 '>
-                {
-                    services.map((service) => <ServiceCard key={service.id} service={service} />)
-                }
-            </div>
-            
-        </div>
-    );
+      {/* Our service  */}
+      <h2 className="text-5xl font-bold text-center mt-16">Our Services</h2>
+      <p className="text-center w-2/5 my-3 mx-auto"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga officiis architecto eveniet aliquid, molestias officiis architecto eveniet aliquid, mole alias? </p>
+      <div className="grid gap-3 my-28 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
+        {services.map((service) => (
+          <ServiceCard key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
