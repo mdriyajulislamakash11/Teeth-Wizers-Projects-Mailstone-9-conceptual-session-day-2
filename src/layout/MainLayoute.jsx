52ec2508a286b6b26633810fa37bfc63c63ce053
Akash <hafejmohammadakash@gmail.com>
2025-05-05 10:12:16 +0600

    footer added

--- src/layout/MainLayoute.jsx
+++ src/layout/MainLayoute.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const MainLayoute = () => {
-    return (
-        <div>
-            {/* Navber */}
-            <Navbar />
+  return (
+    <div>
+      {/* Navber */}
+      <Navbar />
 
-            {/* Outlet */}
-            <div className='w-11/12 mx-auto'>
-                <Outlet />
-            </div>
-            
-        </div>
-    );
+      {/* Outlet */}
+      <div className="w-11/12 mx-auto">
+        <Outlet />
+      </div>
+
+      <div className="mt-11">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default MainLayoute;
